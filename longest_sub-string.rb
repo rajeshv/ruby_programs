2236bdba0cfb8ff0b5f93a0656d8e73c856962db
RajeshV <rajesh.venkat.veedhi@gmail.com>
2022-09-29 08:45:58 +0530

    CHANGE: Adding methods for differnt approaches

--- longest_sub-string.rb
+++ longest_sub-string.rb
@@ -6,7 +6,7 @@ require "byebug"
 #   if last_indx = hello[ret]
 #     m = i - last_indx
 #     if longest < m
-#       longest = m 
+#       longest = m
 #       longest_sub = word[last_indx..i]
 #     end
 #   end
@@ -29,22 +29,44 @@ require "byebug"
 #   longest = hsh[ret] if hsh[ret] > longest
 # end
 
-word = "GEEKSFORGEEKS"
-hsh = {}
-longest = 1
-longest_sub = ""
-margin = 0
-word.chars.each_with_index do |ret, i|
-  if hsh[ret]
-    margin = 1
-    hsh = {}
+def longest_sub_string_using_hash(word)
+  hsh = {}
+  longest = 0
+  longest_sub = ""
+  word.chars.each_with_index do |ret, i|
+    if hsh[ret]
+      hsh[ret] = i - hsh[ret]
+    else
+      hsh[ret] = i
+    end
+    longest = hsh[ret] if hsh[ret] > longest
   end
-  margin += 1
-  if margin > longest
-    longest = margin
-    longest_sub += ret
-  end
-  hsh[ret] = i
+  longest
 end
-p longest
-p longest_sub
+
+p longest_sub_string_using_hash("Mississippi")
+p longest_sub_string_using_hash("longest_sub_string")
+
+def longest_sub_string_using_hash_and_margin(word)
+  return if !word.is_a?(String) && word.is_empty?
+  hsh = {}
+  longest = 1
+  longest_sub = ""
+  margin = 0
+  word.chars.each_with_index do |ret, i|
+    if hsh[ret]
+      margin = 1
+      hsh = {}
+    end
+    margin += 1
+    if margin > longest
+      longest = margin
+      longest_sub += ret
+    end
+    hsh[ret] = i
+  end
+  [longest, longest_sub]
+end
+
+p longest_sub_string_using_hash_and_margin("Mississippi")
+p longest_sub_string_using_hash_and_margin("longest_sub_string")
